4c50453cfec02537070ab6961e5aa41f10ce7d81
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-08-18 12:54:41 +0200

    Added method to check for original numbers

--- ATMOpgaven/CardBank.cs
+++ ATMOpgaven/CardBank.cs
@@ -9,12 +9,41 @@ namespace ATMOpgaven
 {
     public static class CardBank
     {
+        /// <summary>
+        /// Returns a randomly generated card
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="pincode"></param>
+        /// <returns></returns>
         public static CreditCard GenerateNewCard(string name, int pincode)
         {
             Random random = new Random();
-            CreditCard creditCard = new CreditCard(random.Next(1000000000, int.MaxValue), name, random.Next(100, 999), pincode);
-            SaveNewcard(creditCard);
-            return creditCard;
+            int cardNumber = random.Next(1000000000, int.MaxValue);
+            if (IsCardNumberNew(cardNumber))
+            {
+                CreditCard creditCard = new CreditCard(cardNumber, name, random.Next(100, 999), pincode);
+                SaveNewcard(creditCard);
+                return creditCard;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Returns a boolean to see if a cardnumber has been used before
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static bool IsCardNumberNew(int number)
+        {
+            foreach (String item in GetAllCards())
+            {
+                string[] splitItem = item.Split(';');
+                if (number == int.Parse(splitItem[0]))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         /// <summary>
@@ -28,7 +57,7 @@ namespace ATMOpgaven
             string path = Environment.CurrentDirectory + "\\CardBank.txt";
             try
             {
-                File.AppendAllText(path, $"{ConvertCardToString(creditCard)}\n");
+                File.AppendAllText(path, $"{ConvertCardToString(creditCard)}");
             }
             catch (Exception ex)
             {
